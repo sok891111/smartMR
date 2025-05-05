ce8a009e88986e79a6b241b17e7b2aa877aaaf54
sok891111 <sok891111@gmail.com>
2025-05-06 00:19:05 +0900

    Update menu.py

--- menu.py
+++ menu.py
@@ -43,6 +43,11 @@ def menu():
     .stDecoration{
         display:none !important;
     }
+      [class^="_profileContainer"] {
+        display: none !important;
+    }
+
+    
     
         </style>
         """,
